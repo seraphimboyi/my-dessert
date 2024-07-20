459eae076429a61376f434ba9d1036d570ebb352
boyi <seraphimboyi@users.noreply.github.com>
2024-07-20 23:03:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '15': '3.75rem',
+      },
+      writingMode:{
+        'vertical-rl': 'vertical-rl',
+        'vertical-lr': 'vertical-lr',
+        'horizontal-tb': 'horizontal-tb',
+        'sideways-rl': 'sideways-rl',
+        'sideways-lr': 'sideways-lr',
+      }
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.writing-mode-vertical-rl': {
+          'writing-mode': 'vertical-rl',
+        },
+        '.writing-mode-vertical-lr': {
+          'writing-mode': 'vertical-lr',
+        },
+        '.writing-mode-horizontal-tb': {
+          'writing-mode': 'horizontal-tb',
+        },
+        '.writing-mode-sideways-rl': {
+          'writing-mode': 'sideways-rl',
+        },
+        '.writing-mode-sideways-lr': {
+          'writing-mode': 'sideways-lr',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+};
